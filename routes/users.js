0f09f8f8081956e64908fad78b9d6add438b61a1
Pankaj Agade <pankaj.agade@htstechsolutions.com>
2024-10-10 18:08:37 +0530

    change

--- routes/users.js
+++ routes/users.js
@@ -8,29 +8,29 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/get-url', function(req, res, next) {
-  const url = req.query.url;
-  const body = req.query.body;
-  const headers = req.query.headers;
-  const query = req.query.query;
-  const param = req.query.param;
-  const method = req.query.method;
+  const url = req.originalUrl;
+  const body = req.body;
+  const headers = req.headers;
+  const query = req.query;
+  const param = req.param;
+  const method = req.method;
 
-  console.log(url, body, headers, query, param, method);
-  console.log("~~~~~~~~~~~~~~");
+  console.log({url, body, headers, query, param, method});
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
   
   res.status(200).send({success: true});
 });
 
 router.post('/post-url', function(req, res, next) {
-  const url = req.query.url;
-  const body = req.query.body;
-  const headers = req.query.headers;
-  const query = req.query.query;
-  const param = req.query.param;
-  const method = req.query.method;
+  const url = req.url;
+  const body = req.body;
+  const headers = req.headers;
+  const query = req.query;
+  const param = req.param;
+  const method = req.method;
 
-  console.log(url, body, headers, query, param, method);
-  console.log("~~~~~~~~~~~~~~");
+  console.log({url, body, headers, query, param, method});
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
   res.status(200).send({success: true});
 });
